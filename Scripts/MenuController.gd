02a9ed278447969beea21f4a82cfc127c9b4380d
James Meyers <jmmeyers215@gmail.com>
2023-11-05 15:04:33 -0700

    Added options menu

--- Scripts/MenuController.gd
+++ Scripts/MenuController.gd
@@ -1,12 +1,20 @@
 extends Control
 
-
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	pass # Replace with function body.
 
 func _on_option_button_pressed() -> void:
-	pass # Replace with function body.
+	$Options.visible = true
+	$Buttons.visible = false
+	$Title.visible = false
+
+func _on_back_button_pressed():
+	$Options.visible = false
+	$Buttons.visible = true
+	$Title.visible = true
+
 
 func _on_store_button_pressed() -> void:
 	pass # Replace with function body.
+
+
--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,12 +1,11 @@
 extends Control
 
-@onready var dog_cat_button : CheckButton = get_node("DogCatToggle/CheckButton")
+@onready var dog_cat_button : CheckButton = get_node("OptionsPanel/DogCatToggle/CheckButton")
 func _ready() -> void:
 	Global.dog_or_cat = true
 
-func _on_check_button_toggled(toggled_on: bool) -> void:
+func _on_check_button_toggled(toggled_on):
 	if dog_cat_button.button_pressed:
 		Global.dog_or_cat = false
 	else:
 		Global.dog_or_cat = true
-
