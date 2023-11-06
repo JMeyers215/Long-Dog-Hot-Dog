e5b9597d7634104e34c878a491af29b760803db6
James Meyers <jmmeyers215@gmail.com>
2023-11-05 23:25:25 -0700

    Removed Save Game Functionality

--- Scripts/PauseScreen.gd
+++ Scripts/PauseScreen.gd
@@ -1,11 +1,7 @@
 extends Control
 
-
-
 func _on_title_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/start.tscn")
 
-
 func _on_quit_button_pressed() -> void:
-	#Global.save_game()
 	get_tree().quit()
