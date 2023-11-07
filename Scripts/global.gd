783b2632fa1e1f9850134c78ca39730757a63b63
James Meyers <jmmeyers215@gmail.com>
2023-11-06 18:01:31 -0700

    Added basic save functionality to Global

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -7,14 +7,40 @@ var total_count : int = 0
 var high_score : int = 0
 
 #options variables
-var is_cat_or_dog : String = "Dog"
+var dog_or_cat : bool 
+var music_option : int = 1
+var sound_option : int = 1
 
+#defines the save path
+const SAVE_PATH = "user://LDHD.json"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("Persist", true)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func save_game():
+	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	
+	var global = get_node("/root/Global")
+	
+	var save_dict = {
+		global = {
+			total_count = int(total_count),
+			high_score = int(high_score),
+			dog_or_cat = bool(dog_or_cat),
+			music_option = int(music_option),
+			sound_option = int(sound_option)
+		}
+	}
+	
+	file.store_line(JSON.stringify(save_dict))
+
+func load_game():
+	
 	pass
 
 func _notification(what: int) -> void:
