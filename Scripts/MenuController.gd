61d0566999dad124cda431c636e4a4959a61eb4b
James Meyers <jmmeyers215@gmail.com>
2023-11-09 14:19:35 -0700

    Added a Button Sound to Main Menu

--- Scripts/MenuController.gd
+++ Scripts/MenuController.gd
@@ -1,20 +1,24 @@
 extends Control
 
 func _on_start_button_pressed() -> void:
+	$"../ButtonNoise".play()
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_option_button_pressed() -> void:
+	$"../ButtonNoise".play()
 	$Options.visible = true
 	$Buttons.visible = false
 	$Title.visible = false
 
 func _on_back_button_pressed():
+	$"../ButtonNoise".play()
 	$Store.visible = false
 	$Options.visible = false
 	$Buttons.visible = true
 	$Title.visible = true
 
 func _on_store_button_pressed() -> void:
+	$"../ButtonNoise".play()
 	$Store.visible = true
 	$Buttons.visible = false
 	$Title.visible = false
@@ -22,6 +26,7 @@ func _on_store_button_pressed() -> void:
 
 
 func _on_quit_button_pressed() -> void:
+	$"../ButtonNoise".play()
 	Global.save_game()
 	get_tree().quit()
 
---
+++ Scripts/music.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer
+
+@onready var music_control = get_node("/root/MusicPlayer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
