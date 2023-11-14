b6d45314fd7ea357d3532ba84e26ee7e0e539555
James Meyers <jmmeyers215@gmail.com>
2023-11-13 19:59:04 -0700

    Changed Credits, Added Music Looping

--- Scripts/music.gd
+++ Scripts/music.gd
@@ -1,11 +1,11 @@
 extends AudioStreamPlayer
 
-@onready var music_control = get_node("/root/music")
+@onready var music_control = get_node("/root/MusicPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play()
-
+	music_control.stream.loop = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
